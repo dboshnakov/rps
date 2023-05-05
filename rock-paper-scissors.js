7c85e939cf9aa67d22dcce39658847e135c322a6
Diyan Boshnakov <111239673+dboshnakov@users.noreply.github.com>
2023-05-05 22:49:43 +0300

    well... I didn't commit often, but it's done

---
+++ rock-paper-scissors.js
@@ -0,0 +1,73 @@
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 1; i < 6; i++) {
+        let result = playRound();
+        if (result.includes("win")) {
+            playerWins ++;
+        } else {
+            computerWins ++;
+        }
+        console.log(`Player wins: ${playerWins}. Computer wins: ${computerWins}`);
+    }
+    if (playerWins > computerWins) {
+        console.log(`End result: You win! ${playerWins}:${computerWins}`);
+    } else {
+        console.log(`End result: Computer wins! ${playerWins}:${computerWins}`);
+    }
+    if (confirm("Play a new game?")) {
+        game();
+    } else {
+        return "See you soon!";
+    }
+}
+
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    //compare the choices and determine the winner (or draw)
+    if (computerChoice === playerChoice) {
+        return (`Both you and the computer picked ${playerChoice}. Draw.`)
+    } else if (computerChoice === "rock" && playerChoice === "paper") {
+        return("Paper beats rock. You win!");
+    } else if (computerChoice === "rock" && playerChoice === "scissors") {
+        return("Rock beats scissors. You lost!");
+    } else if (computerChoice === "paper" && playerChoice === "rock") {
+        return("Paper beats rock. You lost!");
+    } else if (computerChoice === "paper" && playerChoice === "scissors") {
+        return("Scissors beats paper. You win!");
+    } else if (computerChoice === "scissors" && playerChoice === "rock") {
+        return("Rock beats scissors. You win!");
+    } else if (computerChoice === "scissors" && playerChoice === "paper") {
+        return("Scissors beats paper. You lose!");
+    }
+}
+
+function getPlayerChoice() {
+    //prompt question to the user
+    const choice = prompt("Type out your weapon of choice: \rrock \rpaper \rscissors");
+    // handle case where input of the user is null (closing the prompt with escape/cancel)
+    if (choice === null) {
+        //show an alert pop-up indicating the input is not valid
+        alert("Invalid input.");
+        //trigger the function again
+        return getPlayerChoice();
+    // convert the user's input to lowercase & compare to the expected input options
+    // if true - return the user's input in lowercase 
+    } else if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") { 
+        return choice.toLowerCase();
+    } else {
+        //if not true - show an alert pop-up (same as in row 14)
+        alert("Invalid input.");
+        //trigger the function again
+        return getPlayerChoice();
+    }
+}
+
+function getComputerChoice() {
+    //get a random number between 1 and 3
+    const computerChoice =  Math.floor(Math.random() * (4 - 1)+ 1);
+    //convert the random number to one of the expected input options
+    return (computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors");
+}
