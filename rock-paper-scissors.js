f69af64bc225e545969c4a121a36ace88ef14d03
Diyan Boshnakov <111239673+dboshnakov@users.noreply.github.com>
2023-06-11 20:04:22 +0300

    Add selection markers & fix mobile display issues

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,10 +3,13 @@ let computerScore = 0;
 let roundsToWin = 3;
 const roundsInput = document.querySelector("#rounds");
 roundsInput.textContent = roundsToWin;
-const choice1 = document.querySelector(".rock");
-const choice2 = document.querySelector(".paper");
-const choice3 = document.querySelector(".scissors");
+const choice1 = document.querySelector("img.rock");
+const choice2 = document.querySelector("img.paper");
+const choice3 = document.querySelector("img.scissors");
 const choices = document.querySelector(".choices");
+const rockMark = document.querySelector(".selected.rock");
+const paperMark = document.querySelector(".selected.paper");
+const scissorsMark = document.querySelector(".selected.scissors");
 const game = document.querySelector('.game');
 const outcome = document.querySelector('#outcome');
 const score = document.querySelector('#score');
@@ -14,13 +17,18 @@ const end = document.querySelector('#end');
 const intro = document.querySelector('.main');
 const start = document.querySelector('.initial');
 const restart = document.querySelector('.restart'); 
+let computerChoice;
+let playerChoice;
 
-
+enableChoices();
 
 function playRound(playerChoice) {
     //get each participant's choice by triggering the respective functions
     let computerChoice = getComputerChoice();
-    
+    clearChoice();
+    markChoice("player",playerChoice);
+    markChoice("computer",computerChoice);
+
     if (computerChoice === playerChoice) {
         outcome.textContent = `Both you and the computer picked ${playerChoice}. Draw.`
     } else if (computerChoice === "rock" && playerChoice === "paper") {
@@ -42,9 +50,52 @@ function playRound(playerChoice) {
         outcome.textContent = "Scissors beats paper. You lost!";
         computerScore++;
     }
-    score.textContent = `Player ${playerScore} : ${computerScore} Computer`;
+    score.textContent = `${playerScore} : ${computerScore}`;
 
     endGame(checkWinner(playerScore,computerScore,roundsToWin));
+}
+
+
+function markChoice(player, selection) {
+    if (player === "player") {
+        switch (selection) {
+            case 'rock':
+                console.log("Player ROCK");
+                rockMark.classList.add("player");
+                break;
+            case 'paper':
+                console.log("Player PAPER");
+                paperMark.classList.add("player");
+                break;
+            case 'scissors':
+                console.log("Player SCISSORS");
+                scissorsMark.classList.add("player");
+                break;
+        }
+    } else if (player === "computer") {
+        switch (selection) {
+            case 'rock':
+                console.log("Computer ROCK");
+                rockMark.classList.add("computer");
+                break;
+            case 'paper':
+                console.log("Computer PAPER");
+                paperMark.classList.add("computer");
+                break;
+            case 'scissors':
+                console.log("Computer SCISSORS");
+                scissorsMark.classList.add("computer");
+        }
+    }
+}
+
+function clearChoice() {
+    rockMark.classList.remove("player");
+    paperMark.classList.remove("player");
+    scissorsMark.classList.remove("player");
+    rockMark.classList.remove("computer");
+    paperMark.classList.remove("computer");
+    scissorsMark.classList.remove("computer");
 }
 
 function endGame(winner) {
@@ -95,7 +146,7 @@ function startGame(roundsToWin) {
     enableChoices();
     end.textContent =`First to ${roundsToWin} out of ${(roundsToWin*2)-1}`;
     outcome.textContent = ` `;
-    score.textContent = `Player ${playerScore} : ${computerScore} Computer`;
+    score.textContent = `${playerScore} : ${computerScore}`;
 }
 
 function newGame() {
@@ -109,9 +160,6 @@ function disableChoices() {
     choice1.setAttribute("onclick", false);
     choice2.setAttribute("onclick", false);
     choice3.setAttribute("onclick", false);
-    //choice1.classList.add("disabled");
-    //choice2.classList.add("disabled");
-    //choice3.classList.add("disabled");
     choices.classList.add("disabled");
 }
 
@@ -119,9 +167,6 @@ function enableChoices() {
     choice1.setAttribute("onclick", "playRound('rock')");
     choice2.setAttribute("onclick", "playRound('paper')");
     choice3.setAttribute("onclick", "playRound('scissors')");
-    //choice1.classList.remove("disabled");
-    //choice2.classList.remove("disabled");
-    //choice3.classList.remove("disabled");
     choices.classList.remove("disabled");
 }
 
@@ -130,11 +175,12 @@ function clearFields() {
     score.textContent = ``;
     end.textContent = ``;
     restart.classList.add("hidden");
+    clearChoice();
 }
 
 function getComputerChoice() {
     //get a random number between 1 and 3
-    const computerChoice =  Math.floor(Math.random() * (4 - 1)+ 1);
+    let computerChoice =  Math.floor((Math.random() * 3) + 1);
     //convert the random number to one of the expected input options
     return (computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors");
 }
